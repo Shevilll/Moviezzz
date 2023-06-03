46ea43e374168ab62089adaf7e6c8829c3a5e126
Shevilll <ahmadfaraz00710@gmail.com>
2023-06-03 10:44:34 +0530

    updates

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,36 @@
-import { data } from "../server/functions";
-
+import { useState } from "react";
+import { GetMovies } from "../server/functions";
 export default function App() {
-    return <button onClick={data}>Get</button>;
+    const [page, setPage] = useState(1);
+    const [query, setQuery] = useState("One");
+
+    return (
+        <>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">
+                        Moviezzz
+                    </a>
+                    <form className="d-flex" role="search">
+                        <input
+                            className="form-control me-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                            onChange={(e) => setQuery(e.target.value)}
+                        />
+                        <input
+                            className="form-control me-2"
+                            type="search"
+                            placeholder="Page"
+                            aria-label="Search"
+                            onChange={(e) => setPage(e.target.value)}
+                            defaultValue={1}
+                        />
+                    </form>
+                </div>
+            </nav>
+            <GetMovies page={page ? page : 1} query={query ? query : "One"} />
+        </>
+    );
 }
--- src/server/functions.js
+++ src/server/functions.js
@@ -1,13 +1,40 @@
 import axios from "axios";
-export function data() {
-    axios
-        .get(
-            "https://api.themoviedb.org/3/search/movie?&api_key=0e0301590d89e103d9115358b1ffae7e&page=1&query=demon"
-        )
-        .then((res) => {
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+import { useState, useEffect } from "react";
+
+export function GetMovies({ page, query }) {
+    const API = process.env.REACT_APP_API_KEY;
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axios
+            .get(
+                `https://api.themoviedb.org/3/search/movie?&api_key=${API}&page=${page}&query=${query}`
+            )
+            .then((res) => setData([res.data]));
+    }, [page, query, API]);
+    console.log(data[0]);
+    return (
+        <>
+            <div className="mainbody">
+                {data.map((item, index) =>
+                    item.results.map((titles, id) =>
+                        titles.poster_path ? (
+                            <div key={id} className="mainbox">
+                                <p>{titles.title}</p>
+                                <img
+                                    src={
+                                        "https://image.tmdb.org/t/p/w1280" +
+                                        titles.poster_path
+                                    }
+                                    width={"300px"}
+                                    height={"400px"}
+                                    id="image"
+                                    alt={titles.title}
+                                ></img>
+                            </div>
+                        ) : null
+                    )
+                )}
+            </div>
+        </>
+    );
 }
