ad0be2aabcfc8685b12c0ea21e0e4bd0af4df9f8
Shevilll <ahmadfaraz00710@gmail.com>
2023-06-03 17:34:39 +0530

    main done

--- src/client/App.js
+++ src/client/App.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import { GetMovies } from "../server/functions";
+import { pages } from "../server/functions";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MovieDetails from "../server/MovieDetails";
+
 export default function App() {
     const [page, setPage] = useState(1);
-    const [query, setQuery] = useState("One");
+    const [query, setQuery] = useState("One Piece");
 
     return (
         <>
             <nav className="navbar bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">
+                    <a className="navbar-brand" id="Moviezzz" href="/">
                         Moviezzz
                     </a>
                     <form className="d-flex" role="search">
@@ -17,20 +21,56 @@ export default function App() {
                             type="search"
                             placeholder="Search"
                             aria-label="Search"
-                            onChange={(e) => setQuery(e.target.value)}
-                        />
-                        <input
-                            className="form-control me-2"
-                            type="search"
-                            placeholder="Page"
-                            aria-label="Search"
-                            onChange={(e) => setPage(e.target.value)}
-                            defaultValue={1}
+                            id="searchbar"
+                            onChange={(e) => {
+                                setQuery(e.target.value);
+                                setPage(1);
+                            }}
                         />
                     </form>
                 </div>
             </nav>
-            <GetMovies page={page ? page : 1} query={query ? query : "One"} />
+
+            <Router>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <div className="navigation">
+                                    <button
+                                        className="navbutton"
+                                        onClick={() => setPage(page - 1)}
+                                        disabled={page === 1}
+                                    >
+                                        {"<"}
+                                    </button>
+                                    <label>{page}</label>
+                                    <button
+                                        className="navbutton"
+                                        onClick={() => setPage(page + 1)}
+                                        disabled={page === pages}
+                                    >
+                                        {">"}
+                                    </button>
+                                </div>
+                                <GetMovies
+                                    page={page ? page : 1}
+                                    query={query ? query : "One Piece"}
+                                />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/about"
+                        element={
+                            <>
+                                <MovieDetails />
+                            </>
+                        }
+                    />
+                </Routes>
+            </Router>
         </>
     );
 }
