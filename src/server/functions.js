5c5069c1d824043a76e81d5745e4d74f98d2c48a
Shevilll <ahmadfaraz00710@gmail.com>
2023-06-03 17:34:57 +0530

    functions done

--- src/server/functions.js
+++ src/server/functions.js
@@ -1,34 +1,53 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function GetMovies({ page, query }) {
     const API = process.env.REACT_APP_API_KEY;
     const [data, setData] = useState([]);
+    const navigate = useNavigate();
     useEffect(() => {
         axios
             .get(
                 `https://api.themoviedb.org/3/search/movie?&api_key=${API}&page=${page}&query=${query}`
             )
-            .then((res) => setData([res.data]));
+            .then((res) => {
+                setData([res.data]);
+            });
     }, [page, query, API]);
-    console.log(data[0]);
+    handleExtras(data);
     return (
         <>
             <div className="mainbody">
                 {data.map((item, index) =>
-                    item.results.map((titles, id) =>
-                        titles.poster_path ? (
-                            <div key={id} className="mainbox">
-                                <p>{titles.title}</p>
+                    item.results.map((result, id) =>
+                        result.poster_path ? (
+                            <div
+                                key={id}
+                                className="mainbox"
+                                onClick={() => {
+                                    navigate("/about", {
+                                        state: [
+                                            "https://image.tmdb.org/t/p/w1280" +
+                                                result.backdrop_path,
+                                            result.title,
+                                            result.overview,
+                                            result.popularity,
+                                            result.release_date,
+                                        ],
+                                    });
+                                }}
+                            >
+                                <p>{result.title}</p>
                                 <img
                                     src={
                                         "https://image.tmdb.org/t/p/w1280" +
-                                        titles.poster_path
+                                        result.poster_path
                                     }
                                     width={"300px"}
                                     height={"400px"}
                                     id="image"
-                                    alt={titles.title}
+                                    alt={result.title}
                                 ></img>
                             </div>
                         ) : null
@@ -38,3 +57,14 @@ export function GetMovies({ page, query }) {
         </>
     );
 }
+let pages;
+function handleExtras(data) {
+    if (data && data.length > 0) {
+        pages = data[0].total_pages;
+    }
+    const searchbar = document.getElementById("searchbar");
+    if (searchbar) {
+        searchbar.disabled = false;
+    }
+}
+export { pages };
