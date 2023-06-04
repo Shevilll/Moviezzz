e4c5fa0b11faf5e421f870f25f6033603fe90198
Shevilll <ahmadfaraz00710@gmail.com>
2023-06-04 18:03:20 +0530

    adding watchlist

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import { GetMovies } from "../server/functions";
 import { pages } from "../server/functions";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useNavigate,
+} from "react-router-dom";
 import MovieDetails from "../server/MovieDetails";
+import Watchlist from "../server/Watchlist";
 
 export default function App() {
     const [page, setPage] = useState(1);
@@ -10,33 +16,13 @@ export default function App() {
 
     return (
         <>
-            <nav className="navbar bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" id="Moviezzz" href="/">
-                        Moviezzz
-                    </a>
-                    <form className="d-flex" role="search">
-                        <input
-                            className="form-control me-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                            id="searchbar"
-                            onChange={(e) => {
-                                setQuery(e.target.value);
-                                setPage(1);
-                            }}
-                        />
-                    </form>
-                </div>
-            </nav>
-
             <Router>
                 <Routes>
                     <Route
                         path="/"
                         element={
                             <>
+                                <NavBar setPage={setPage} setQuery={setQuery} />
                                 <div className="navigation">
                                     <button
                                         className="navbutton"
@@ -65,12 +51,57 @@ export default function App() {
                         path="/about"
                         element={
                             <>
+                                <NavBar setPage={setPage} setQuery={setQuery} />
                                 <MovieDetails />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/watchlist"
+                        element={
+                            <>
+                                <NavBar setPage={setPage} setQuery={setQuery} />
+                                <Watchlist />
                             </>
                         }
                     />
                 </Routes>
             </Router>
+        </>
+    );
+}
+function NavBar({ setPage, setQuery }) {
+    const navigate = useNavigate();
+    return (
+        <>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" id="Moviezzz" href="/">
+                        Moviezzz
+                    </a>
+                    <form className="d-flex" role="search">
+                        <input
+                            className="form-control me-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                            id="searchbar"
+                            autoComplete="off"
+                            onChange={(e) => {
+                                setQuery(e.target.value);
+                                setPage(1);
+                            }}
+                        />
+                    </form>
+                    <button
+                        className="navbutton"
+                        onClick={() => navigate("/watchlist")}
+                    >
+                        Watchlist
+                    </button>
+                </div>
+            </nav>
+            ;
         </>
     );
 }
