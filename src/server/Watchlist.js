0cee241b6f744515eefd63274c6ebfe5dc2e7b37
Shevilll <ahmadfaraz00710@gmail.com>
2023-06-05 18:04:10 +0530

    working on watchlist

--- src/server/Watchlist.js
+++ src/server/Watchlist.js
@@ -1,21 +1,45 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { HeartButton } from "./functions";
 
 export default function Watchlist() {
-    return <h1>Watchlist</h1>;
+    const [details, setDetails] = useState();
+    useEffect(() => {
+        axios
+            .get("http://localhost:8000/get")
+            .then((res) => setDetails(res.data));
+    }, []);
+    console.log(details);
+    if (details !== undefined && details !== []) {
+        return (
+            <>
+                <div className="mainbody">
+                    {details.map((result, index) => (
+                        <div className="mainbox" key={index}>
+                            <p id="title">{result.title}</p>
+                            <img
+                                src={result.poster_path}
+                                width={"300px"}
+                                height={"400px"}
+                                id="image"
+                                title={result.title}
+                                alt={result.title}
+                            />
+                            <HeartButton result={result} />
+                        </div>
+                    ))}
+                </div>
+            </>
+        );
+    } else {
+        return <h1 style={{ textAlign: "center" }}>...</h1>;
+    }
 }
-export function getData() {
-    axios
-        .get("http://localhost:8000/get")
-        // .then((res) => console.log(res.data))
-        .catch((err) => console.log(err));
-}
-getData();
 export function postData(title, poster_path) {
     axios
         .post("http://localhost:8000/post", {
-            Movietitle: title,
-            posterpath: poster_path,
+            title: title,
+            poster_path: poster_path,
         })
         .then((response) => {
             console.log("Data sent successfully");
@@ -29,8 +53,8 @@ export function postData(title, poster_path) {
 export function deleteData(title, poster_path) {
     axios
         .post("http://localhost:8000/delete", {
-            Movietitle: title,
-            posterpath: poster_path,
+            title: title,
+            poster_path: poster_path,
         })
         .then((response) => {
             console.log("Data deleted successfully");
