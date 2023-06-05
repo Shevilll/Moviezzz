dd5bf537d55591e65431002db41e4baad5652d48
Shevilll <ahmadfaraz00710@gmail.com>
2023-06-05 17:18:54 +0530

    stuck now

--- src/server/Watchlist.js
+++ src/server/Watchlist.js
@@ -2,26 +2,41 @@ import React, { useEffect, useRef } from "react";
 import axios from "axios";
 
 export default function Watchlist() {
-    const isMountedRef = useRef(false);
-
-    useEffect(() => {
-        if (!isMountedRef.current) {
-            isMountedRef.current = true;
-            axios
-                .post("http://localhost:8000/", { hi: "hi" })
-                .then((response) => {
-                    console.log("Data sent successfully");
-                    console.log("Response:", response.data);
-                })
-                .catch((error) => {
-                    console.log("Error sending data:", error);
-                });
-        }
-    }, []);
-
     return <h1>Watchlist</h1>;
 }
-// axios
-//     .get("http://localhost:8000")
-//     .then((res) => console.log(res.data))
-//     .catch((err) => console.log(err));
+export function getData() {
+    axios
+        .get("http://localhost:8000/get")
+        // .then((res) => console.log(res.data))
+        .catch((err) => console.log(err));
+}
+getData();
+export function postData(title, poster_path) {
+    axios
+        .post("http://localhost:8000/post", {
+            Movietitle: title,
+            posterpath: poster_path,
+        })
+        .then((response) => {
+            console.log("Data sent successfully");
+            console.log("Response:", response.data);
+        })
+        .catch((error) => {
+            console.log("Error sending data:", error);
+        });
+}
+
+export function deleteData(title, poster_path) {
+    axios
+        .post("http://localhost:8000/delete", {
+            Movietitle: title,
+            posterpath: poster_path,
+        })
+        .then((response) => {
+            console.log("Data deleted successfully");
+            console.log("Response:", response.data);
+        })
+        .catch((error) => {
+            console.log("Error sending data:", error);
+        });
+}
